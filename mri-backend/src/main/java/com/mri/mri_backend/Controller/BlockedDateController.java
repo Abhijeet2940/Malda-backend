429acf71967c5689892ed46022d5375f1157756c
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2026-05-12 00:06:19 +0530

    Change CORS origin for BlockedDateController

--- mri-backend/src/main/java/com/mri/mri_backend/Controller/BlockedDateController.java
+++ mri-backend/src/main/java/com/mri/mri_backend/Controller/BlockedDateController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/blocked-dates")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://maldarailwayinstute.in")
 public class BlockedDateController {
 
     private final BlockedDateService blockedDateService;
