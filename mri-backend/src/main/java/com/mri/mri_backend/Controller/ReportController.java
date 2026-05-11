5a965ae18359afc22714a5fdcb74ea57c6cd6b22
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2026-05-12 01:57:26 +0530

    Update CORS origin in ReportController

--- mri-backend/src/main/java/com/mri/mri_backend/Controller/ReportController.java
+++ mri-backend/src/main/java/com/mri/mri_backend/Controller/ReportController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/reports")
-@CrossOrigin(origins = "http://maldarailwayinstitute.in")
+@CrossOrigin(origins = "http://www.maldarailwayinstitute.in")
 public class ReportController {
 
     private final ReportService reportService;
