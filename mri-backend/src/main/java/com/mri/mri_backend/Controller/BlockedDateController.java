e3a057c118774e3e2d0553e496002ee2545aac6c
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2026-05-12 01:04:59 +0530

    Update BlockedDateController.java

--- mri-backend/src/main/java/com/mri/mri_backend/Controller/BlockedDateController.java
+++ mri-backend/src/main/java/com/mri/mri_backend/Controller/BlockedDateController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/blocked-dates")
-@CrossOrigin(origins = "http://maldarailwayinstute.in")
+@CrossOrigin(origins = "http://maldarailwayinstitute.in")
 public class BlockedDateController {
 
     private final BlockedDateService blockedDateService;
